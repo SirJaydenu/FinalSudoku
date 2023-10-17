27e4ec5a8eef20d4cbd919ef67efb0c2ee28971d
SirJaydenu <Jaydenu06@icloud.com>
2023-10-17 11:43:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/finalsudoku/Game.java
@@ -0,0 +1,46 @@
+package com.example.finalsudoku;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Game extends AppCompatActivity{
+    View shine;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(Game.this, "1st Step", Toast.LENGTH_SHORT).show();
+        shine = findViewById(R.id.shine);
+        shineAnimation();
+    }
+
+    private void shineAnimation() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.left_right);
+        shine.startAnimation(anim);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                shine.startAnimation(anim);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    public void openSudoku(){
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+    }
+}
